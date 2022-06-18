e967a5d65d51d30b2e9a5aa93bfe8e675d1d45d5
sumitsasidharan <s.sumit196@gmail.com>
2022-06-18 13:00:39 +0530

    added new event comps & styles

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,22 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+
 function EventDetailPage() {
+   const router = useRouter();
+   
+   const eventId = router.query.eventId;
+   const event = getEventById(eventId);
+
+   if (!event) {
+      return (
+         <h1>no event found!</h1>
+      )
+   }
+
    return (
       <div>
          <h1>Event Detail Page</h1>
+         
       </div>
    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../dummy-data';
 
 function HomePage() {
+   const featuredEvents = getFeaturedEvents();
+   console.log(featuredEvents)
+
   return (
-    <div>the Home Page</div>
-  )
+     <div>
+        <EventList items={featuredEvents} />
+     </div>
+  );
 }
 
 export default HomePage
