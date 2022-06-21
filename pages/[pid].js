8d47dd881f1f2b219e2bf151db5e6bcb3bceb725
sumitsasidharan <s.sumit196@gmail.com>
2022-06-21 22:41:47 +0530

    updated pid & index page

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,11 +1,59 @@
-import { Fragment, useEffect } from "react";
+import path from 'path';
+import fs from 'fs/promises';
 
-function ProductDetail() {
+import { Fragment } from 'react';
+
+function ProductDetail(props) {
+   const { loadedProduct } = props;
+
+   // if fallback is 'blocking' in getstaticpaths, then below check isn't needed, nextjs will check if data is loaded/ fetched
+   if (!loadedProduct) {
+      return <p>Loading...</p>
+   }
+
+   return (
+      <Fragment>
+         <h1>{loadedProduct.title}</h1>
+         <p>{loadedProduct.description}</p>
+      </Fragment>
+   );
+}
+
+async function getData() {
+   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+   const jsonData = await fs.readFile(filePath);
+   const data = JSON.parse(jsonData);
+
+   return data;
+}
+
+export async function getStaticProps(context) {
+   const { params } = context;
+   const productId = params.pid;
+
+   const data = await getData();
+
+   const product = data.products.find(product => product.id === productId);
    
-   return <Fragment>
-      <h1>{title}</h1>
-      <p>{description}</p>
-   </Fragment>
+   return {
+      props: {
+         loadedProduct: product
+      },
+   };
+}
+
+export async function getStaticPaths() {
+   const data = await getData();
+
+   const ids = data.products.map(product => product.id);
+
+   const pathsWithParams = ids.map(id => ({params: { pid: id}}))
+
+   return {
+      paths: pathsWithParams
+      
+      fallback: 'blocking', // true option generates the page in real time when needed
+   };
 }
 
 export default ProductDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import path from 'path';
 import fs from 'fs/promises';
 
+import Link from 'next/link';
+
 function HomePage(props) {
    const { products } = props;
 
   return (
-    <ul>
-      { products.map(product => <li key={product.id}>{product.title}</li>)}
-    </ul>
+     <ul>
+        {products.map((product) => (
+           <li key={product.id}>
+              <Link href={`/${product.id}`}>{product.title}</Link>
+           </li>
+        ))}
+     </ul>
   );
 }
 
 export async function getStaticProps() {
-   console.log(process.cwd())
    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
    const jsonData = await fs.readFile(filePath);
    const data = JSON.parse(jsonData);
