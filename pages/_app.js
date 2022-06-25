9602f462f8837044a7c8e9aeadd631830b6afb04
sumitsasidharan <s.sumit196@gmail.com>
2022-06-25 22:33:52 +0530

    optimizing with Head & Image

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
-import Layout from '../components/layout/layout'
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-     <Layout>
-        <Component {...pageProps} />
-     </Layout>
-  );
+   return (
+      <Layout>
+         <Head>
+            {/* generic title for every page unless it's overwritten */}
+            <title>NextJS Events</title>
+            <meta name='description' content='NextJS Events' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+         </Head>
+         <Component {...pageProps} />
+      </Layout>
+   );
 }
 
-export default MyApp
+export default MyApp;
