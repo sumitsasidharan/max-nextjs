9200f12fb28ca61c9d0653d92068e43959fc053a
sumitsasidharan <s.sumit196@gmail.com>
2022-06-25 10:12:04 +0530

    updated routing tuts

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
-function LastSalesPage() {
-   const [users, setUsers] = useState();
-   const [isLoading, setIsLoading] = useState(false);
-   console.log(users);
+function LastSalesPage(props) {
+   const [users, setUsers] = useState(props.users);
+   // const [isLoading, setIsLoading] = useState(false);
+
+   const url = 'https://jsonplaceholder.typicode.com/users';
+
+   // SERVER + CLIENT SIDE RENDERING
+   const { data, error } = useSWR(url, (url) =>
+      fetch(url).then((res) => res.json())
+   );
+
+   console.log(data);
    useEffect(() => {
-      setIsLoading(true);
-      fetch('https://jsonplaceholder.typicode.com/users')
-         .then((response) => response.json())
-         .then((data) => {
-            setUsers(data);
-            setIsLoading(false);
-         });
-   }, []);
+      if (data) {
+         setUsers(data);
+      }
+   }, [data]);
+
+   // useEffect(() => {
+   //    setIsLoading(true);
+   //    fetch('https://jsonplaceholder.typicode.com/users')
+   //       .then((response) => response.json())
+   //       .then((data) => {
+   //          setUsers(data);
+   //          setIsLoading(false);
+   //       });
+   // }, []);
 
    // iterating through an object
    // const transformedSales = []
@@ -20,12 +35,20 @@ function LastSalesPage() {
    //    transformedSales.push({id: key, username: data[key].username})
    // }
 
-   if (isLoading) {
-      return <h1>Loading...</h1>;
+   // if (isLoading) {
+   //    return <h1>Loading...</h1>;
+   // }
+
+   // if (!users) {
+   //    return <h1>Still Loading.......</h1>;
+   // }
+
+   if (error) {
+      return <h1>failed to load...</h1>;
    }
 
-   if (!users) {
-      return <h1>Still Loading.......</h1>;
+   if (!data || !users) {
+      return <h1>Loading...</h1>;
    }
 
    return (
@@ -40,6 +63,18 @@ function LastSalesPage() {
          ))}
       </ul>
    );
+}
+
+export async function getStaticProps() {
+   const response = await fetch('https://jsonplaceholder.typicode.com/users');
+   const data = await response.json();
+         
+   return {
+      props: {
+         users: data
+      },
+      revalidate: 10
+   }
 }
 
 export default LastSalesPage;
