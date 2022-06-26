0752d1784a440f187cc38bdfe4ff18d412a4e864
sumitsasidharan <s.sumit196@gmail.com>
2022-06-26 21:32:27 +0530

    updated input comments

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,30 +1,46 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+   const emailInputRef = useRef();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
+   function registrationHandler(event) {
+      event.preventDefault();
 
-  return (
-    <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
-          <button>Register</button>
-        </div>
-      </form>
-    </section>
-  );
+      const enteredEmail = emailInputRef.current.value;
+
+      fetch('/api/newsletter', {
+         method: 'POST',
+         body: JSON.stringify({ email: enteredEmail }),
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      })
+         .then((response) => response.json())
+         .then((data) => console.log(data));
+
+      // fetch user input (state or refs)
+      // optional: validate input
+      // send valid data to API
+   }
+
+   return (
+      <section className={classes.newsletter}>
+         <h2>Sign up to stay updated!</h2>
+         <form onSubmit={registrationHandler}>
+            <div className={classes.control}>
+               <input
+                  ref={emailInputRef}
+                  type="email"
+                  id="email"
+                  placeholder="Your email"
+                  aria-label="Your email"
+               />
+               <button>Register</button>
+            </div>
+         </form>
+      </section>
+   );
 }
 
 export default NewsletterRegistration;
---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,31 @@
+function handler(req, res) {
+   const eventId = req.query.eventId;
+
+   if (req.method === 'POST') {
+      const { email, name, text } = req.body;
+
+      if (!email.includes('@') || !name || name.trim() === '' || !text || text.trim() === '') {
+         res.status(422).json({ message: 'Invalid input.'});
+         return;
+      }
+      
+      const newComment = {
+         id: new Date().toISOString(),
+         email, name, text
+      }
+      console.log(newComment);
+      res.status(201).json({ message: 'Added comment.', comment: newComment})
+   }
+
+   if (req.method === 'GET') {
+      const dummyList = [
+         { id: 'c1', name: 'sumit', text: 'hi, ponnu'},
+         { id: 'c2', name: 'ponnu', text: 'hello, sasi'},
+         { id: 'c3', name: 'jane', text: 'guys, m cumming'},
+      ]
+
+      res.status(200).json({ comments: dummyList});
+   }
+}
+
+export default handler;
