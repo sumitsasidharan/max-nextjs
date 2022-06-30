fbed94b9e144f1ef72424c736c98c69e4712b0e1
sumitsasidharan <s.sumit196@gmail.com>
2022-06-30 23:14:50 +0530

    created posts comps

---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import classes from './logo.module.css';
+
+function Logo() {
+   return (
+      <div className={classes.logo}>Sumit's Blog</div>
+   )
+}
+
+export default Logo;
---
+++ components/posts/post-item.js
@@ -0,0 +1,9 @@
+import classes from './post-item.module.css';
+
+function PostItem() {
+  return (
+    <div>PostItem</div>
+  )
+}
+
+export default PostItem
